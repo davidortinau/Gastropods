d9f2992fe82ee30556020f1ed37f8c9bf4390ce3
David Ortinau <david.ortinau@microsoft.com>
2018-12-04 20:41:31 -0600

    added view model and now passing data via querystring to it

--- Gastropod/Pages/CameraPage.xaml.cs
+++ Gastropod/Pages/CameraPage.xaml.cs
@@ -3,30 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Gastropod.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Internals;
 using Xamarin.Forms.Xaml;
 
 namespace Gastropod
 {
-    [Preserve]
-    [QueryProperty("Payload", "payload")]
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class CameraPage : ContentPage
-	{
-		public CameraPage ()
-		{
-			InitializeComponent ();
-		}
-
-        private string _payload;
-        public string Payload
+    public partial class CameraPage : ContentPage
+    {
+        public CameraPage()
         {
-            get { return _payload; }
-            set { _payload = value;
-                PayloadLabel.Text = _payload;
-            }
+            InitializeComponent();
+            BindingContext = new CameraViewModel();
         }
-	}
+
+
+    }
 }
---
+++ Gastropod/ViewModels/CameraViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace Gastropod.ViewModels
+{
+    [QueryProperty("Payload", "payload")]
+    public class CameraViewModel : INotifyPropertyChanged
+    {
+        public CameraViewModel()
+        {
+        }
+
+        private string _payload;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Payload
+        {
+            get { return _payload; }
+            set
+            {
+                SetAndRaisePropertyChanged(ref _payload, value);
+            }
+        }
+
+        protected void SetAndRaisePropertyChanged<TRef>(
+            ref TRef field, TRef value, [CallerMemberName] string propertyName = null)
+        {
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetAndRaisePropertyChangedIfDifferentValues<TRef>(
+            ref TRef field, TRef value, [CallerMemberName] string propertyName = null)
+            where TRef : class
+        {
+            if (field == null || !field.Equals(value))
+            {
+                SetAndRaisePropertyChanged(ref field, value, propertyName);
+            }
+        }
+    }
+}
